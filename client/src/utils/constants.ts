9f6a3c57b226f08f4f964a4e2775b37f43501dc1
Harpica <harpy.ka@gmail.com>
2023-04-06 14:24:49 +0400

    feat: socials github

---
+++ client/src/utils/constants.ts
@@ -0,0 +1,3 @@
+export const BASE_URL = process.env.REACT_APP_BASE_URL || 'localhost';
+export const SERVER_PORT = process.env.REACT_APP_SERVER_PORT || '5004';
+export const CLIENT_PORT = process.env.REACT_APP_CLIENT_PORT || '3000';
---
+++ client/src/views/LoginView.tsx
@@ -0,0 +1,33 @@
+import { LoginSocialGithub, IResolveParams } from 'reactjs-social-login';
+import { GithubLoginButton } from 'react-social-login-buttons';
+import { BASE_URL, CLIENT_PORT } from '../utils/constants';
+
+const LoginView = () => {
+    return (
+        <div>
+            <LoginSocialGithub
+                client_id={process.env.REACT_APP_SOCIALS_GITHUB_ID || ''}
+                client_secret={
+                    process.env.REACT_APP_SOCIALS_GITHUB_SECRET || ''
+                }
+                // redirect_uri={`http://${BASE_URL}`}
+                redirect_uri={`http://${BASE_URL}:${CLIENT_PORT}`}
+                // onLoginStart={onLoginStart}
+                // onLogoutSuccess={onLogoutSuccess}
+                onResolve={({ provider, data }: IResolveParams) => {
+                    // setProvider(provider);
+                    // setProfile(data);
+                    console.log('provader', provider);
+                    console.log('data', data);
+                }}
+                onReject={(err: any) => {
+                    console.log(err);
+                }}
+            >
+                <GithubLoginButton />
+            </LoginSocialGithub>
+        </div>
+    );
+};
+
+export default LoginView;
