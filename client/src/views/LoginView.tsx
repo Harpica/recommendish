2e1ac25633cd6ed2e4a922d8f4c220803d33c984
Harpica <harpy.ka@gmail.com>
2023-04-11 12:50:22 +0400

    feat: log in with twitter

--- client/src/views/LoginView.tsx
+++ client/src/views/LoginView.tsx
@@ -1,5 +1,12 @@
-import { LoginSocialGithub, IResolveParams } from 'reactjs-social-login';
-import { GithubLoginButton } from 'react-social-login-buttons';
+import {
+    LoginSocialGithub,
+    IResolveParams,
+    LoginSocialTwitter,
+} from 'reactjs-social-login';
+import {
+    GithubLoginButton,
+    TwitterLoginButton,
+} from 'react-social-login-buttons';
 import { BASE_URL, CLIENT_PORT } from '../utils/constants';
 
 const LoginView = () => {
@@ -27,6 +34,25 @@ const LoginView = () => {
             >
                 <GithubLoginButton />
             </LoginSocialGithub>
+            <LoginSocialTwitter
+                isOnlyGetToken
+                client_id={process.env.REACT_APP_TWITTER_V2_APP_SECRET || ''}
+                redirect_uri={`http://${BASE_URL}:${CLIENT_PORT}`}
+                onLoginStart={() => {
+                    console.log('start login');
+                }}
+                onResolve={({ provider, data }: IResolveParams) => {
+                    // setProvider(provider);
+                    // setProfile(data);
+                    console.log('provider', provider);
+                    console.log('data', data);
+                }}
+                onReject={(err: any) => {
+                    console.log(err);
+                }}
+            >
+                <TwitterLoginButton />
+            </LoginSocialTwitter>
         </div>
     );
 };
